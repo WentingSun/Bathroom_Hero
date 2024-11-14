7b9b410a41e916813fca09f323ad213c332030e6
WENTING SUN <45490316+WentingSun@users.noreply.github.com>
2024-11-14 16:09:26 +0000

    Adding some interface and base class

---
+++ Assets/Scripts/Interface&BaseClass/IMusicItem.cs
@@ -0,0 +1,4 @@
+public interface IMusicItem{
+    public void BeSelected();
+    public void PlayMusic();
+}
---
+++ Assets/Scripts/ItemScript/MopGuitar.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MopGuitar : MusicItem
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
