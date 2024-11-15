aac9a870523a8ecc52ed3ea991d77ff181c3d339
XYZhang <94607868+ddaiyy@users.noreply.github.com>
2024-11-15 14:27:27 +0000

    Add attribute in BaseMusicItem

--- Assets/Scripts/Interface&BaseClass/BaseMusicItem.cs
+++ Assets/Scripts/Interface&BaseClass/BaseMusicItem.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BaseMusicItem : MonoBehaviour,IMusicItem
 {
+    [SerializeField] public TestVRinput input;
     public virtual void BeSelected()
     {
         throw new System.NotImplementedException();
--- Assets/Scripts/ItemScript/MopGuitar.cs
+++ Assets/Scripts/ItemScript/MopGuitar.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MopGuitar : BaseMusicItem
 {
-    [SerializeField]public TestVRinput input;
+    
     public int testNum;
     // Start is called before the first frame update
     void Start()
