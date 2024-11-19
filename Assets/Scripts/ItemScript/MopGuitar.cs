0fb9e06c84252536b62a3e4bc335442719a4362f
WENTING SUN <45490316+WentingSun@users.noreply.github.com>
2024-11-19 20:57:30 +0000

    implement the mop moment logic

--- Assets/Scripts/ItemScript/MopGuitar.cs
+++ Assets/Scripts/ItemScript/MopGuitar.cs
@@ -4,11 +4,31 @@ using UnityEngine;
 
 public class MopGuitar : BaseMusicItem
 {
+    [SerializeField]Transform Mop;
+    [SerializeField]Transform FixPoint;
+    [SerializeField]bool isSelected;
+    public Vector3 rotationOffset;
+    float distanseOfLeftHand=>getDistanceOfHand();
     
     
     private void OnTriggerEnter(Collider other){
-        
         AudioManager.Instance.PlaySound("TestSound");
+    }
+
+    public override void BeSelected()
+    {
+        isSelected = true;
+        Debug.Log("Mop be selected");
+    }
+
+    public override void UnSelected()
+    {
+        isSelected = false;
+        Debug.Log("Mop be unselected");
+    }
+
+    private float getDistanceOfHand(){
+        return Vector3.Distance(FixPoint.position,input.GetLeftHandPosition().position);
     }
 
     // Start is called before the first frame update
@@ -20,6 +40,10 @@ public class MopGuitar : BaseMusicItem
     // Update is called once per frame
     void Update()
     {
-        
+        if(isSelected){
+            Vector3 direction = input.GetLeftHandPosition().position -FixPoint.position;
+            Mop.rotation = Quaternion.LookRotation(direction) * Quaternion.Euler(rotationOffset);
+        }
+        // Debug.Log(distanseOfLeftHand);
     }
 }
