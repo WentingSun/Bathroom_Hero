e122d5fb0a861eecff24703f972bb0ec0fde6140
WENTING SUN <45490316+WentingSun@users.noreply.github.com>
2024-11-20 11:59:56 +0000

    Create Guitar Mode Structure

--- Assets/Scripts/ItemScript/MopGuitar.cs
+++ Assets/Scripts/ItemScript/MopGuitar.cs
@@ -7,6 +7,10 @@ public class MopGuitar : BaseMusicItem
     [SerializeField]Transform Mop;
     [SerializeField]Transform FixPoint;
     [SerializeField]bool isSelected;
+    [SerializeField] private GuitarMode guitarMode;
+    [SerializeField] private float ModeARange;
+    [SerializeField] private float ModeBRange;
+    [SerializeField] private float ModeCRange;
     public Vector3 rotationOffset;
     float distanseOfLeftHand=>getDistanceOfHand();
     
@@ -34,16 +38,40 @@ public class MopGuitar : BaseMusicItem
     // Start is called before the first frame update
     void Start()
     {
-        
+        CheckAttribute();
     }
+
+private void CheckAttribute()
+{
+    if (!(ModeCRange > ModeARange && ModeBRange > ModeARange && ModeCRange > ModeBRange))
+    {
+        Debug.Log("Range Mode Attribute has problem");
+    }
+
+    
+}
+
 
     // Update is called once per frame
     void Update()
     {
+        if(distanseOfLeftHand < ModeARange){
+            guitarMode=GuitarMode.A;
+        }else if(distanseOfLeftHand < ModeBRange){
+            guitarMode=GuitarMode.B;
+        }else if(distanseOfLeftHand < ModeCRange){
+            guitarMode=GuitarMode.C;
+        }
         if(isSelected){
             Vector3 direction = input.GetLeftHandPosition().position -FixPoint.position;
             Mop.rotation = Quaternion.LookRotation(direction) * Quaternion.Euler(rotationOffset);
         }
         // Debug.Log(distanseOfLeftHand);
     }
+}
+
+public enum GuitarMode{
+    A,
+    B,
+    C
 }
