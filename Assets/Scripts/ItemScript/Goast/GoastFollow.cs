659a08dc740a2f35b5bbbff48d13607c939083b0
XYZhang <94607868+ddaiyy@users.noreply.github.com>
2024-11-29 02:29:58 +0000

    SkullFollow1.0
    Skull will move following the movement of the player

--- Assets/Scripts/ItemScript/Goast/GoastFollow.cs
+++ Assets/Scripts/ItemScript/Goast/GoastFollow.cs
@@ -1,43 +1,134 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class GoastFollow : MonoBehaviour
 {
+    //Head
+    public Transform cameraFollowTransform; // 渲染摄像机引用
 
-    public Transform cameraTransform; // 摄像机引用
+    public Transform vrBasicMainCameraTransform;
+    public Transform vrRControllerTransform;
+    public Transform vrLControllerTransform;
+    //transform引用整体
+    [SerializeField]private Transform headTransform;//头部
+    [SerializeField]private Transform lHandTransform;//左手
+    InputDevice leftHandController;
+    [SerializeField]private Transform rHandTransform;//右手
+    [SerializeField]private Transform bodyTransform;
     [SerializeField]private Vector3 offset; // 位置偏移量
     [SerializeField]private Quaternion initialRotationDifference; // 初始旋转偏移量
+
+    Vector3 LeftControllerOffset;
+    Transform OLeftControllerTransform;
+    Vector3 initPosition;
+
+    //Hand
+
  
     float unchangableX;
 
-    void Awake(){
-        // 计算初始位置偏移量
-        offset = transform.position - cameraTransform.position;
 
-        // 计算初始旋转的偏移量（镜像旋转）
-        transform.rotation = initialRotationDifference;
-        //
-        transform.position=cameraTransform.position+offset;
+    void Awake(){
+        
     }
     void Start()
-    {
-        
+    { 
+        // 计算初始位置偏移量
+        offset = transform.position - cameraFollowTransform.position;
         //
         unchangableX=transform.position.x;
+        //
+        transform.rotation = initialRotationDifference;
+        //
+        //transform.position=cameraTransform.position+offset;
+        //TransformFollow();
+        //originalLeftOffset=vrBasicMainCameraTransform.position-vrLControllerTransform.position;
+        OLeftControllerTransform=GameObject.Find("[Left Controller] Model Parent").transform;
+        initPosition = OLeftControllerTransform.position;
+        
     }
 
     void Update()
     {
-        //transform.position=cameraTransform.position+offset;
-        // 获取摄像机的当前位置
-        Vector3 cameraPosition = cameraTransform.position;
-
-        // 保持Cube的X轴位置不变，Y轴和Z轴位置跟随摄像机
-        transform.position = new Vector3(unchangableX, cameraPosition.y + offset.y, cameraPosition.z + offset.z);
+        HeadTransformFollow(); 
+        BasicTransformFollow(bodyTransform);
+        //HandTransformFollow(RHandTransform);
+        
+        // leftOffset=vrBasicMainCameraTransform.position-vrLControllerTransform.position;
+        // Debug.Log("OFFSET:"+leftOffset);
+        LeftHandTransformFollow();
+        
+    }
+    void LeftHandMoveDecect(){
+        vrLControllerTransform=GameObject .Find("[Left Controller] Model Parent").transform;
+        if(initPosition!=vrLControllerTransform.position){
+            LeftControllerOffset=vrLControllerTransform.position-cameraFollowTransform.position;
+        }else{
+            Debug.Log("没有变化");
+        }
+    
+        
+    }
+    void LeftHandTransformFollow(){
+        //得到偏移LeftControllerOffset
+        LeftHandMoveDecect();
+        Debug.Log(LeftControllerOffset);
+        // 获取骷髅
+        lHandTransform.position=new Vector3(headTransform.position.x-LeftControllerOffset.x,
+            headTransform.position.y+LeftControllerOffset.y,
+            headTransform.position.z-LeftControllerOffset.z);
+        
+        //originalLeftOffset=originalLeftOffset;
         
         // 获取摄像机的旋转
-        Quaternion cameraRotation = cameraTransform.rotation;
+        Quaternion cameraRotation = cameraFollowTransform.rotation;
+
+        // 提取摄像机的欧拉角
+        Vector3 cameraEulerAngles = cameraRotation.eulerAngles;
+
+        // 只保留 Y 轴的旋转，去掉 X 和 Z 轴的旋转
+        float yRotationAngle = cameraEulerAngles.y;
+
+        // 创建绕 Y 轴的旋转
+        Quaternion yRotation = Quaternion.Euler(0, -yRotationAngle, 0);
+
+        // 应用到 Cube 的旋转
+        lHandTransform.rotation = yRotation;
+
+        
+        }
+
+    void BasicTransformFollow(Transform Basic){
+        // 获取摄像机的当前位置
+        Vector3 cameraPosition = cameraFollowTransform.position;
+        // 保持Cube的X轴位置不变，Y轴和Z轴位置跟随摄像机
+        transform.position = new Vector3(unchangableX, cameraPosition.y + offset.y, cameraPosition.z + offset.z);
+        // 获取摄像机的旋转
+        Quaternion cameraRotation = cameraFollowTransform.rotation;
+
+        // 提取摄像机的欧拉角
+        Vector3 cameraEulerAngles = cameraRotation.eulerAngles;
+
+        // 只保留 Y 轴的旋转，去掉 X 和 Z 轴的旋转
+        float yRotationAngle = cameraEulerAngles.y;
+
+        // 创建绕 Y 轴的旋转
+        Quaternion yRotation = Quaternion.Euler(0, -yRotationAngle, 0);
+
+        // 应用到 Cube 的旋转
+        Basic.rotation = yRotation;
+    }
+
+    void HeadTransformFollow(){
+        // 获取摄像机的当前位置
+        Vector3 cameraPosition = cameraFollowTransform.position;
+        // 保持Cube的X轴位置不变，Y轴和Z轴位置跟随摄像机
+        transform.position = new Vector3(unchangableX, cameraPosition.y + offset.y, cameraPosition.z + offset.z);
+         // 获取摄像机的旋转
+        Quaternion cameraRotation = cameraFollowTransform.rotation;
 
         // 提取摄像机的欧拉角
         Vector3 cameraEulerAngles = cameraRotation.eulerAngles;
@@ -50,6 +141,6 @@ public class GoastFollow : MonoBehaviour
         Quaternion finalRotation = yRotation * xzRotation;
 
         // 应用到 Cube 的旋转
-        transform.rotation = finalRotation;
+        headTransform.rotation = finalRotation;
     }
 }
---
+++ Assets/Scripts/ItemScript/Goast/SkullFollow.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkullFollow : MonoBehaviour
+{
+    [Header("VR头显位置")]
+    public Transform vrBasicMainCameraTransform;
+    public Transform vrRControllerTransform;
+    public Transform vrLControllerTransform;
+
+    [Header("Skull")]
+    public Transform skullHead;
+    public Transform skullRHand;
+    public Transform skullLHand;
+    public Transform skullBody;
+
+    [Header("摄像机初始位置")]
+    public Transform cameraFollowTransform;
+    public Vector3 offset;
+    public Vector3 rightHandOffset;
+    public Vector3 leftHandOffset;
+    // Start is called before the first frame update
+    [Header("自己设定的手柄偏移")]
+    public Vector3 BothHandOffset;
+    void Start()
+    {
+        // 计算初始位置偏移量
+        offset = transform.position - cameraFollowTransform.position;
+        vrRControllerTransform=GameObject.Find("[Right Controller] Model Parent").transform;
+        vrLControllerTransform=GameObject.Find("[Left Controller] Model Parent").transform;
+        rightHandOffset=skullRHand.position-vrRControllerTransform.position;
+        leftHandOffset=skullLHand.position-vrLControllerTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HeadTransformFollow(); 
+        BasicTransformFollow();
+        vrRControllerTransform=GameObject.Find("[Right Controller] Model Parent").transform;
+        vrLControllerTransform=GameObject.Find("[Left Controller] Model Parent").transform;
+        HandTransformFollow(skullLHand,vrLControllerTransform,leftHandOffset);
+        HandTransformFollow(skullRHand,vrRControllerTransform,rightHandOffset);
+    }
+    void HandTransformFollow(Transform SkullHand,Transform ControllerTransform,Vector3 HandOffset){
+        // 获取摄像机的当前位置
+        //Vector3 cameraPosition = cameraFollowTransform.position;
+        Vector3 ContollerPosition=ControllerTransform.position;
+        // 保持Cube的X轴位置不变，Y轴和Z轴位置跟随摄像机
+        SkullHand.position = new Vector3(ContollerPosition.x + HandOffset.x+BothHandOffset.x, ContollerPosition.y + HandOffset.y+BothHandOffset.y, ContollerPosition.z + HandOffset.z+BothHandOffset.z);
+         // 获取摄像机的旋转
+        Quaternion ContollerRotation = ControllerTransform.rotation;
+
+        // 提取摄像机的欧拉角
+        Vector3 controllerEulerAngles = ContollerRotation.eulerAngles;
+
+        // 分离 Y 轴旋转（反向）和 X-Z 平面旋转（同步）
+        Quaternion yRotation = Quaternion.Euler(0, controllerEulerAngles.y, 0); // 绕Y 轴旋转
+        Quaternion xzRotation = Quaternion.Euler(controllerEulerAngles.x, 0, controllerEulerAngles.z); // X 和 Z 不变
+
+        // 组合旋转
+        Quaternion finalRotation = yRotation * xzRotation;
+
+        // 应用到 Cube 的旋转
+        SkullHand.rotation = finalRotation;
+    }
+    void HeadTransformFollow(){
+        // 获取摄像机的当前位置
+        Vector3 cameraPosition = cameraFollowTransform.position;
+        // 保持Cube的X轴位置不变，Y轴和Z轴位置跟随摄像机
+        transform.position = new Vector3(cameraPosition.x + offset.x, cameraPosition.y + offset.y, cameraPosition.z + offset.z);
+         // 获取摄像机的旋转
+        Quaternion cameraRotation = cameraFollowTransform.rotation;
+
+        // 提取摄像机的欧拉角
+        Vector3 cameraEulerAngles = cameraRotation.eulerAngles;
+
+        // 分离 Y 轴旋转（反向）和 X-Z 平面旋转（同步）
+        Quaternion yRotation = Quaternion.Euler(0, cameraEulerAngles.y, 0); // 绕Y 轴旋转
+        Quaternion xzRotation = Quaternion.Euler(cameraEulerAngles.x, 0, cameraEulerAngles.z); // X 和 Z 不变
+
+        // 组合旋转
+        Quaternion finalRotation = yRotation * xzRotation;
+
+        // 应用到 Cube 的旋转
+        skullHead.rotation = finalRotation;
+    }
+     void BasicTransformFollow(){
+        // 获取摄像机的当前位置
+        Vector3 cameraPosition = cameraFollowTransform.position;
+        // 保持Cube的X轴位置不变，Y轴和Z轴位置跟随摄像机
+        transform.position = new Vector3(cameraPosition.x + offset.x, cameraPosition.y + offset.y, cameraPosition.z + offset.z);
+        // 获取摄像机的旋转
+        Quaternion cameraRotation = cameraFollowTransform.rotation;
+
+        // 提取摄像机的欧拉角
+        Vector3 cameraEulerAngles = cameraRotation.eulerAngles;
+
+        // 只保留 Y 轴的旋转，去掉 X 和 Z 轴的旋转
+        float yRotationAngle = cameraEulerAngles.y;
+
+        // 创建绕 Y 轴的旋转
+        Quaternion yRotation = Quaternion.Euler(0, yRotationAngle, 0);
+
+        // 应用到 Cube 的旋转
+        skullBody.rotation = yRotation;
+    }
+}
