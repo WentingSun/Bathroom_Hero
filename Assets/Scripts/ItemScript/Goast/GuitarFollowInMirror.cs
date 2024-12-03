60d0f7de7ae56827c40f4fde8b0176a60dfc1ce5
XYZhang <94607868+ddaiyy@users.noreply.github.com>
2024-12-03 04:50:16 +0000

    ItemsFollowInMirror
    Guitar exists some problems
    Tubelight is done

---
+++ Assets/Scripts/ItemScript/Goast/GuitarFollowInMirror.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuitarFollowInMirror : MonoBehaviour
+{
+    [SerializeField] private Transform Mop;         // 需要调整位置的物体
+    [SerializeField] private Transform FixPoint;    // 固定点
+    [SerializeField] private Transform rightHandCollider; // 右手碰撞器
+    public Vector3 rotationOffset;                 // 旋转的额外偏移
+    public Transform skullLeftHand;
+    public Transform skullRightHand;
+
+    private void Start()
+    {
+        StartCoroutine(UpdatePositionAndRotation());
+    }
+
+    private IEnumerator UpdatePositionAndRotation()
+    {
+        while (true)
+        {
+            // 将 Mop 位置同步到 FixPoint
+            Mop.position = FixPoint.position;
+
+            // 设置 Mop 的朝向为左手位置
+            Vector3 direction = skullLeftHand.position - FixPoint.position;
+            Mop.rotation = Quaternion.LookRotation(direction) * Quaternion.Euler(rotationOffset);
+
+            // 同步右手碰撞器的位置
+            rightHandCollider.position = skullRightHand.position;
+
+            yield return null;
+        }
+    }
+
+}
